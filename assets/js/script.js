d901f35f59fd9e3092cde7a87ba18d2d040a0685
Oliveiralucaz <lucasdeoliveira050@gmail.com>
2023-05-02 21:52:09 -0300

    adicionando arquivos do projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,43 @@
+// Este código foi escrito por Lucas de Oliveira (oliveira.dev.br)
+//selecionando elementos da página com os IDs ou classes correspondentes
 let form = document.getElementById('formulario');
 let botao = document.getElementById('enviar');
 let local = document.querySelector('.inicio');
 let erro = document.querySelector('.erro');
 let sucesso = document.querySelector('.sucesso');
 let nota = document.querySelector('.nota');
-// console.log(erro);
 
+// Quando o formulário é enviado (sumbit), esta função é chamada
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    let resultado = formData.get('valor');
-    if (resultado == null) {
-        erro.classList.toggle("none");
-        local.classList.toggle("none");
-        if (erro.classList.contains('animacao')){
-            erro.classList.remove('animacao');
-            erro.classList.toggle('animacao');
-        }else{
-            erro.classList.add('animacao');
-        }
-        
-        
-        
-    } else {
-        
-        local.classList.toggle("none");
-        sucesso.classList.toggle("none");
-        if (sucesso.classList.contains('animacao')){
-            sucesso.classList.remove('animacao');
-            sucesso.classList.toggle('animacao');
-        }else{
-            sucesso.classList.add('animacao');
-        }
-        nota.innerHTML = resultado;
-    }
+e.preventDefault(); // Previne o envio padrão do formulário
+const formData = new FormData(form); // Cria um objeto FormData com os dados do formulário
+let resultado = formData.get('valor'); // Obtém o valor do campo com o nome "valor" do formulário
+if (resultado == null) { // Se o resultado for nulo, exibe a mensagem de erro
+erro.classList.toggle("none"); // Torna a div com classe "erro" visível
+local.classList.toggle("none"); // Torna a div com classe "inicio" invisível
+if (erro.classList.contains('animacao')){ // Se a div de erro já estiver animada
+erro.classList.remove('animacao'); // Remove a classe "animacao" para poder reiniciar a animação
+erro.classList.toggle('animacao'); // Adiciona novamente a classe "animacao" para reiniciar a animação
+}else{
+erro.classList.add('animacao'); // Adiciona a classe "animacao" para animar a div de erro
+}
+} else { // Se o resultado não for nulo, exibe a mensagem de sucesso
+local.classList.toggle("none"); // Torna a div com classe "inicio" invisível
+sucesso.classList.toggle("none"); // Torna a div com classe "sucesso" visível
+if (sucesso.classList.contains('animacao')){ // Se a div de sucesso já estiver animada
+sucesso.classList.remove('animacao'); // Remove a classe "animacao" para poder reiniciar a animação
+sucesso.classList.toggle('animacao'); // Adiciona novamente a classe "animacao" para reiniciar a animação
+}else{
+sucesso.classList.add('animacao'); // Adiciona a classe "animacao" para animar a div de sucesso
+}
+nota.innerHTML = resultado; // Insere o resultado na div com classe "nota"
+}
 });
 
+// Esta função é chamada quando o botão "voltar" é clicado na mensagem de erro
 function voltar(){
-    
-    erro.classList.toggle("none");
-    local.classList.toggle("none");
+erro.classList.toggle("none"); // Torna a div com classe "erro" invisível
+local.classList.toggle("none"); // Torna a div com classe "inicio" visível novamente
 }
 
 
